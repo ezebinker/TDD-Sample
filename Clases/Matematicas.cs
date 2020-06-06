01b9dfd99663dc50867f679f0b63a9e8c785e248
Ezequiel Binker <ezebinker@gmail.com>
2020-06-06 15:43:04 -0300

    Primer Commit

---
+++ Clases/Matematicas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public static class Matematicas
+    {
+        public static int Sumar (int num1, int num2)
+        {
+            return num1+num2;
+        }
+        public static int Dividir(int num1,int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ UnitTestProject1/PruebasMate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Clases;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class PruebasMate
+    {
+        [TestMethod]
+        public void Sumar()
+        {
+            Assert.AreEqual(5, Matematicas.Sumar(2, 3));
+            Assert.AreNotEqual(4, Matematicas.Sumar(2, 3));
+            Assert.AreEqual(10, Matematicas.Sumar(10, 0));
+            Assert.AreNotEqual(11, Matematicas.Sumar(11, 3));
+        }
+        [TestMethod,ExpectedException(typeof(System.DivideByZeroException))]
+        public void DividirXCeroYError()
+        {
+            Matematicas.Dividir(2, 0);
+        }
+        [TestMethod]
+        public void Dividir()
+        {
+            Matematicas.Dividir(3, 2);
+        }
+    }
+}
